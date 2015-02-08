9b9c1e0c8a8b2df708c7dec4e40046011d2887fa
Conor Hayes <ch1987@live.ie>
2015-02-08 14:04:37 +0000

    Added library for 0mq integration. Created context and subscriber.

--- MonitorAuctions.py
+++ MonitorAuctions.py
@@ -1,1 +1,18 @@
 __author__ = 'Conor'
+
+import zmq
+
+context = zmq.Context()
+SUBSCRIBER_ADDRESS = 'tcp://127.0.0.1:1111'
+
+
+class MonitorAuctions:
+
+    @staticmethod
+    def initialize_subscriber():
+        subscriber = context.socket(zmq.SUB)
+        subscriber.connect(SUBSCRIBER_ADDRESS)
+
+if __name__ == '__main__':
+    monitor = MonitorAuctions()
+    monitor.initialize_subscriber()
