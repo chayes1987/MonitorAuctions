6cb676837873ae409eac83e4706042d9c1fab37e
Conor Hayes <ch1987@live.ie>
2015-02-08 14:09:14 +0000

    Added subscription to all addresses

--- MonitorAuctions.py
+++ MonitorAuctions.py
@@ -3,7 +3,6 @@ __author__ = 'Conor'
 import zmq
 
 context = zmq.Context()
-SUBSCRIBER_ADDRESS = 'tcp://127.0.0.1:1111'
 
 
 class MonitorAuctions:
@@ -11,7 +10,18 @@ class MonitorAuctions:
     @staticmethod
     def initialize_subscriber():
         subscriber = context.socket(zmq.SUB)
-        subscriber.connect(SUBSCRIBER_ADDRESS)
+        subscriber.connect('tcp://127.0.0.1:1000')
+        subscriber.connect('tcp://127.0.0.1:1001')
+        subscriber.connect('tcp://127.0.0.1:1010')
+        subscriber.connect('tcp://127.0.0.1:1011')
+        subscriber.connect('tcp://127.0.0.1:1100')
+        subscriber.connect('tcp://127.0.0.1:1101')
+        subscriber.connect('tcp://127.0.0.1:1110')
+        subscriber.connect('tcp://127.0.0.1:1111')
+        subscriber.connect('tcp://127.0.0.1:2000')
+        subscriber.connect('tcp://127.0.0.1:2001')
+        subscriber.connect('tcp://127.0.0.1:2010')
+        subscriber.setsockopt(zmq.SUBSCRIBE, '')
 
 if __name__ == '__main__':
     monitor = MonitorAuctions()
