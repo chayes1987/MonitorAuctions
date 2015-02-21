4fdaf98e95942cd3d735a43c1a6dd89fca2746b4
Conor Hayes <ch1987@live.ie>
2015-02-21 21:47:31 +0000

    Added log timestamp

--- MonitorAuctions.py
+++ MonitorAuctions.py
@@ -5,6 +5,7 @@ __author__ = 'Conor'
 # Firebase -> https://pypi.python.org/pypi/python-firebase/1.2
 
 from firebase import firebase
+from datetime import datetime
 import zmq
 
 context = zmq.Context()
@@ -23,24 +24,26 @@ class MonitorAuctions:
 
     def update_ui(self, message):
         auction_id = self.parse_message(message, '<id>', '</id>')
+        data = {'_id': auction_id, 'log': message, 'log_date': datetime.now()}
         try:
-            my_firebase.post('/auctions/' + auction_id + '/logs', {'log': message})
+            my_firebase.post('/auctions/' + auction_id + '/logs', data)
             print('Log updated...')
         except Exception:
             pass
 
     def initialize_subscriber(self):
         subscriber = context.socket(zmq.SUB)
-        subscriber.connect('tcp://172.31.32.20:1001')
-        subscriber.connect('tcp://172.31.32.22:1010')
-        subscriber.connect('tcp://172.31.32.21:1011')
-        subscriber.connect('tcp://172.31.32.23:1100')
-        subscriber.connect('tcp://172.31.32.29:1101')
-        subscriber.connect('tcp://172.31.32.23:1110')
-        subscriber.connect('tcp://172.31.32.23:1111')
-        subscriber.connect('tcp://172.31.32.24:2000')
-        subscriber.connect('tcp://172.31.32.25:2001')
-        subscriber.connect('tcp://172.31.32.28:2010')
+        subscriber.connect('tcp://172.31.32.20:2000')
+        subscriber.connect('tcp://172.31.32.21:2100')
+        subscriber.connect('tcp://172.31.32.22:2200')
+        subscriber.connect('tcp://172.31.32.23:2300')
+        subscriber.connect('tcp://172.31.32.23:2350')
+        subscriber.connect('tcp://172.31.32.23:2360')
+        subscriber.connect('tcp://172.31.32.23:2370')
+        subscriber.connect('tcp://172.31.32.24:2400')
+        subscriber.connect('tcp://172.31.32.25:2500')
+        subscriber.connect('tcp://172.31.32.28:2800')
+        subscriber.connect('tcp://172.31.32.29:2900')
         subscriber.setsockopt(zmq.SUBSCRIBE, str.encode(''))
         print('Subscribed to all commands & events...')
 
