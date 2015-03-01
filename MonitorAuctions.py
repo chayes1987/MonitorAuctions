428071a4033cd6a6395bf4d2e220425bc6e00acd
Conor Hayes <ch1987@live.ie>
2015-03-01 18:07:28 +0000

    'renamed'

--- MonitorAuctions.py
+++ MonitorAuctions.py
@@ -2,18 +2,19 @@ __author__ = 'Conor'
 
 # The official documentation was consulted for all three 3rd party libraries used
 # ZeroMQ -> https://learning-0mq-with-pyzmq.readthedocs.org/en/latest/pyzmq/patterns/pubsub.html
-# Firebase -> https://pypi.python.org/pypi/python-firebase/1.2
 
-from firebase import firebase
 from datetime import datetime
 import zmq
 
 context = zmq.Context()
-FIREBASE_URL = 'https://auctionapp.firebaseio.com'
-my_firebase = firebase.FirebaseApplication(FIREBASE_URL, authentication=None)
+my_firebase = None
 
 
 class MonitorAuctions:
+
+    def __init__(self, firebase):
+        global my_firebase
+        my_firebase = firebase
 
     @staticmethod
     def parse_message(message, start_tag, end_tag):
@@ -31,28 +32,17 @@ class MonitorAuctions:
         except Exception:
             pass
 
-    def initialize_subscriber(self):
+    def initialize_subscriber(self, addresses):
         subscriber = context.socket(zmq.SUB)
-        subscriber.connect('tcp://172.31.32.20:2000')
-        subscriber.connect('tcp://172.31.32.21:2100')
-        subscriber.connect('tcp://172.31.32.22:2200')
-        subscriber.connect('tcp://172.31.32.23:2300')
-        subscriber.connect('tcp://172.31.32.23:2350')
-        subscriber.connect('tcp://172.31.32.23:2360')
-        subscriber.connect('tcp://172.31.32.23:2370')
-        subscriber.connect('tcp://172.31.32.24:2400')
-        subscriber.connect('tcp://172.31.32.25:2500')
-        subscriber.connect('tcp://172.31.32.28:2800')
-        subscriber.connect('tcp://172.31.32.29:2900')
+
+        for key, address in addresses:
+            subscriber.connect(address)
+
         subscriber.setsockopt(zmq.SUBSCRIBE, str.encode(''))
-        print('Subscribed to all commands & events...')
+        print('SUB: All commands & events...')
 
         while True:
             msg = subscriber.recv()
             m = msg.decode()
-            print(m + ' received...')
+            print('REC: ' + m)
             self.update_ui(m)
-
-if __name__ == '__main__':
-    monitor = MonitorAuctions()
-    monitor.initialize_subscriber()
